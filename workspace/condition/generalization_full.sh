10c8c5f3d9ff3a6151f70ae10abb0f2896883be6
Doven <MTDovent@gmail.com>
2025-01-05 23:43:15 +0800

    exp.

--- workspace/condition/generalization_full.sh
+++
@@ -1,9 +0,0 @@
-accelerate launch \
-  --main_process_port=12345 \
-  --multi_gpu \
-  --num_processes=4 \
-  --gpu_ids='1,2,3,4' \
-  --num_machines=1 \
-  --mixed_precision=bf16 \
-  --dynamo_backend=no \
-  generalization_full.py
